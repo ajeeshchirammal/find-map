0524f7976b7bbf92093a7b8aebbfefb341c4aeb4
ajeesh <ajeesh@flogic.in>
2020-09-26 00:20:53 +0530

    server rendering

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+// end 
+app.use(cors(corsOptions));
 app.use(express.static(__dirname + '/dist/map'));
 
 app.get('/*', function (req, res) {
